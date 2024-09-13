ae0e012cb8e29cb9a83bb303c3769a37d10d48af
Alyson Lima <alysonrafael2005@gmail.com>
2024-09-13 23:55:36 +0000

    commit para reestruturação do JS no ex06

--- ex06/main6.js
+++ ex06/main6.js
@@ -7,7 +7,7 @@ const palavras = [
 let palavra = palavras[Math.floor(Math.random() * palavras.length)];
 let palavraOculta = Array(palavra.length).fill("_");
 let tentativas = 0;
-const maxTentativas = 6;
+const maxTentativas = 9;
 
 const palavraSecretaDiv = document.getElementById("palavraSecreta");
 const letrasDiv = document.getElementById("letras");
@@ -20,59 +20,69 @@ palavraSecretaDiv.innerHTML = palavraOculta.join(" ");
 function desenharForca(erros) {
     switch (erros) {
         case 1:
+            // Base da forca
             ctx.beginPath();
             ctx.moveTo(10, 190);
             ctx.lineTo(150, 190);
             ctx.stroke();
             break;
         case 2:
+            // Poste da forca
             ctx.beginPath();
             ctx.moveTo(80, 190);
             ctx.lineTo(80, 50);
             ctx.stroke();
             break;
         case 3:
+            // Suporte superior
             ctx.beginPath();
             ctx.moveTo(80, 50);
             ctx.lineTo(130, 50);
             ctx.stroke();
             break;
         case 4:
+            // Corda
             ctx.beginPath();
             ctx.moveTo(130, 50);
             ctx.lineTo(130, 70);
             ctx.stroke();
             break;
         case 5:
+            // Cabeça
             ctx.beginPath();
             ctx.arc(130, 80, 10, 0, Math.PI * 2);
             ctx.stroke();
             break;
         case 6:
+            // Tronco
             ctx.beginPath();
             ctx.moveTo(130, 90);
             ctx.lineTo(130, 140);
             ctx.stroke();
             break;
         case 7:
+            // Braço esquerdo
             ctx.beginPath();
             ctx.moveTo(130, 100);
             ctx.lineTo(110, 120);
             ctx.stroke();
             break;
         case 8:
+            // Braço direito
             ctx.beginPath();
             ctx.moveTo(130, 100);
             ctx.lineTo(150, 120);
             ctx.stroke();
             break;
         case 9:
+            // Perna esquerda
             ctx.beginPath();
             ctx.moveTo(130, 140);
             ctx.lineTo(110, 170);
             ctx.stroke();
             break;
         case 10:
+            // Perna direita
             ctx.beginPath();
             ctx.moveTo(130, 140);
             ctx.lineTo(150, 170);
